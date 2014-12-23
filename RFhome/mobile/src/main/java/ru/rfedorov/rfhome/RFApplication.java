7dcb289523f091e6b101ad74d3c0041e858e4112
Roman Fedorov <rfedorov@linkentools.com>
2014-12-23 18:29:59 +0600

    add module wear_tools

--- RFhome/mobile/src/main/java/ru/rfedorov/rfhome/RFApplication.java
+++ RFhome/mobile/src/main/java/ru/rfedorov/rfhome/RFApplication.java
@@ -1,24 +1,11 @@
 package ru.rfedorov.rfhome;
 
-import android.app.Application;
-import android.content.Context;
-import android.preference.PreferenceManager;
-import android.util.Log;
+import ru.rfedorov.wear_tools.BaseApplication;
 
-public class RFApplication extends Application {
-    private static final String TAG = "RFApplication";
-
-    private static Context context;
-
+public class RFApplication extends BaseApplication {
+    @Override
     public void onCreate() {
-        Log.v(TAG, "RFApplication onCreate");
         super.onCreate();
-        RFApplication.context = getApplicationContext();
-        //PreferenceManager.setDefaultValues(getAppContext(), R.layout.settings, false);
         Controller.getInstance();
-    }
-
-    public static Context getAppContext() {
-        return RFApplication.context;
     }
 }
---
+++ RFhome/wear_tools/src/main/java/ru/rfedorov/wear_tools/BaseApplication.java
@@ -0,0 +1,23 @@
+package ru.rfedorov.wear_tools;
+
+import android.app.Application;
+import android.content.Context;
+import android.util.Log;
+
+public class BaseApplication extends Application {
+    private static final String TAG = "RFApplicationBase";
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.v(TAG, "RFApplication onCreate");
+        BaseApplication.context = getApplicationContext();
+        //PreferenceManager.setDefaultValues(getAppContext(), R.layout.settings, false);
+        //Controller.getInstance();
+    }
+
+    public static Context getAppContext() {
+        return BaseApplication.context;
+    }
+}
